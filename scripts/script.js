4602ec93f6482fb239c1148831a88937e268c6a4
VshaunR <vramshaun@gmail.com>
2024-09-26 22:52:13 -0400

    no bugs

--- scripts/script.js
+++ scripts/script.js
@@ -134,62 +134,192 @@ srcArr.forEach((src)=>{
 
 
 
-let list=[];
+// let list=[];
 
-todo.addEventListener('submit',(e)=>{
-  e.preventDefault(e);
+// todo.addEventListener('submit',(e)=>{
+//   e.preventDefault(e);
   
-  if(item.length <6){
-    return;
-  };
-  if(item.value ==""){
-    alert('Enter a Item!')
-    return;
-  }
-  add(item.value)
-  item.value=``;
-  item.focus();
-  render(list)
-  });
+//   if(item.length <6){
+//     return;
+//   };
+//   if(item.value ==""){
+//     alert('Enter a Item!')
+//     return;
+//   }
+//   add(item.value)
+//   item.value=``;
+//   item.focus();
+//   render(list)
+//   });
   
 
-let count=0;
-  function add(item){
-    let obj={
-      item:item,
-      id:count,
-    }
-    list.push(obj)
-    count++
+// let count=0;
+//   function add(item){
+//     let obj={
+//       item:item,
+//       id:count,
+//     }
+//     list.push(obj)
+//     count++
 
-console.log(list)
+// console.log(list)
 
-  };
+//   };
 
 
 
-  function render(list){
-    let HTML= ``
-    let accumulator=''
-      list.forEach((item)=>{
+//   function render(list){
+//     let HTML= ``
+//     let accumulator=''
+//       list.forEach((item)=>{
      
-       let del= document.createElement('button')
-       del.setAttribute('class','delete');
-       del.textContent='Delete'
-        HTML =`
-        <div class= 'render-list'>
-          <div>${item.id}${item.item} <button class = 'delete'id='${item.id}'>${del.textContent}</button></div>
+//        let del= document.createElement('button')
+//        del.setAttribute('class','delete');
+//        del.textContent='Delete'
+//         HTML =`
+//         <div class= 'render-list'>
+//           <div>${item.id}${item.item} <button class = 'delete'id='${item.id}'>${del.textContent}</button></div>
           
-        </div>
+//         </div>
       
-        `
+//         `
    
-        // console.log(del)
-        accumulator += HTML
-        document.querySelector('.delete').addEventListener('click',()=>{
-          list.splice(item.id,1);
-          render(list)
-        });
-      });
-      listsItems.innerHTML= accumulator;
-  }
+//         // console.log(del)
+//         accumulator += HTML
+//         document.querySelector('.delete').addEventListener('click',()=>{
+//           list.splice(item.id,1);
+//           render(list)
+//         });
+//       });
+//       listsItems.innerHTML= accumulator;
+//   }
+
+let arrayObj=[
+
+];
+
+
+
+let todoForm = document.getElementById('todo');
+let userInput = document.getElementById('item');
+// let userDate= document.getElementById('user-date');
+let todoList= document.getElementById('lists-items');
+
+
+
+
+todoForm.addEventListener('submit',(event)=>{
+event.preventDefault()
+if(userInput.value ==""){
+  alert('Submit a Todo!')
+  return
+}
+add(userInput.value);
+userInput.value=``;
+userInput.focus();
+console.log(userInput.value);
+
+
+});
+
+
+
+
+
+
+let count =0;
+function add(userInput){
+let obj={}
+let div= document.createElement('div');
+let input = document.createElement('li');
+
+let del = document.createElement('button');
+input.textContent= userInput;
+div.appendChild(input)
+div.appendChild(del)
+div.setAttribute('class','list-item')
+del.setAttribute('id',count);
+input.setAttribute('id',count);
+
+del.textContent=`Delete`
+
+
+todoList.appendChild(div);
+
+
+obj={
+  item:input.textContent,
+  id:count
+}
+arrayObj.push(obj)
+console.log(arrayObj)
+
+delList(del,input,arrayObj);
+// editList(input,date,arrayObj,count);
+count ++;
+}
+
+
+function delList(del,input){
+  del.addEventListener('click',(e)=>{
+    // console.log(e.target.parentElement.className)
+    let parentClass=input.className;
+
+    console.log(input.className)
+    let childClass=e.target.className;
+    // console.log(e.target.className)
+    if(parentClass==childClass){
+        del.remove(del);
+        input.remove(input);
+      
+        arrayObj.splice(Number(childClass),1)
+        console.log(arrayObj);
+        userInput.focus();
+    }
+  });
+};
+
+let filter = document.getElementById('filter-list')
+let filterForm = document.getElementById('filter-form');
+let filterId= document.getElementById('filter-id');
+let filterDate= document.getElementById('filter-date')
+// filterForm.addEventListener('submit',(event)=>{
+// event.preventDefault();
+
+
+// renderById(filterId.value)
+
+// renderByDate(filterDate.value)
+// });
+
+// function renderById(id){
+//   let div= document.createElement('div');
+
+//   div.setAttribute('class','filter-item')
+// arrayObj.forEach((item)=>{
+//   if(item.id==id){
+//     div.innerHTML =`
+//     <li>${item.item}</li>
+//     <li>${item.date}</li>
+//       <li>${item.id}</li>
+//     `
+//     filter.appendChild(div)
+//   }
+
+// });
+
+  
+// };
+
+// function renderByDate(date){
+//   arrayObj.forEach((item)=>{
+//     if(item.date===date){
+//       filter.innerHTML =`
+//       <li>${item.item}</li>
+//       <li>${item.date}</li>
+    
+//       `
+//     }
+ 
+//   });
+// };
